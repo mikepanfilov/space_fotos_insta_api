aef0c69227c51454196f0cc73dbcc785bfbadc74
mikepanfilov <mikepaf@gmail.com>
2020-06-06 16:47:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Upload space fotos to Instagram
+
+Script downloads space-thematic photos from SpaceX and Hubble repos to Instagram. All using sites API.
+
+## How to install
+
+Python3 should be already installed.
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import pprint as pp
+import requests
+
+def download_image(url, filename):
+    response = requests.get(url)
+    response.raise_for_status()
+    dir = 'images'
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+    filepath = os.path.join(dir,filename)
+    with open (filepath, 'wb') as file:
+        file.write(response.content)
+
+def fetch_spacex_last_lauch():
+    images_url = 'https://api.spacexdata.com/v3/launches/latest'
+    response = requests.get(images_url)
+    response.raise_for_status()
+    image_links = response.json()['links']['flickr_images']
+    for link_number, link in enumerate(image_links):
+        download_image(link, f'spacex{link_number + 1}.jpg')
+
+def get_file_extention(address):
+    return address.split('.')[-1]
+
+def fetch_hubble_images(image_id):
+    images_url = f'http://hubblesite.org/api/v3/image/{image_id}'
+    response = requests.get(images_url)
+    response.raise_for_status()
+    image_links = response.json()['image_files']
+
+    for link_number, link in enumerate(image_links):
+        link_corrected = 'https:' + link['file_url']
+        ext = get_file_extention(link_corrected)
+        download_image(link_corrected, f'hubble_id{image_id}_{link_number + 1}.' + ext)
+
+def main():
+    fetch_spacex_last_lauch()
+    fetch_hubble_images(4673)
+
+if __name__ == '__main__':
+    main()
